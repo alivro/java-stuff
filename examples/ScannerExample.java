77597a57a4bac795b1a574c4dfb2bb01f0b67f53
alivro <alfonso.rojo.03@outlook.com>
2021-12-07 20:39:42 -0600

    examples: Agrega este directorio
    Agrega ejemplos:
    * DialogExample
    * ScannerExample
    * SwitchExample

---
+++ examples/ScannerExample.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ScannerExample {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = sc.nextLine();
+
+        System.out.println("How old are you?");
+        int age = sc.nextInt();
+        // Clean scanner
+        sc.nextLine();
+
+        System.out.println("What is your favorite food?");
+        String food = sc.nextLine();
+
+        System.out.println("Hello " + name);
+        System.out.println("You are " + age + " years old");
+        System.out.println("You like " + food);
+
+        sc.close();
+    }
+	
+}
