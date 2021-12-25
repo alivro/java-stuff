dbd2421ec761f8e050498137dae9f9bec5d39b04
alivro <alfonso.rojo.03@outlook.com>
2021-12-24 19:54:52 -0600

    string/examples: Actualiza contenido
    Correciones y cambios menores:
    * FormatExample.java
    * JoinExample.java
    * SplitExample.java

--- string/examples/FormatExample.java
+++ string/examples/FormatExample.java
@@ -2,29 +2,41 @@ public class FormatExample {
 
 	public static void main(String[] args) {
 		FormatExample.example1();
-		FormatExample.example2();
-		FormatExample.example3();
+		//FormatExample.example2();
+		//FormatExample.example3();
 	}
 
 	public static void example1() {
-        	String entero = String.format("%d", 11037);  
-        	String flotante = String.format("%f %.8f", 3.141592653589793, 3.141592653589793);
-        	String notacionCientificaComputarizada = String.format("%e %E", 0.00001618033988749894, 0.00001618033988749894);
-        	String notacionCientificaGeneral = String.format("%g %G", 0.00002718281828459045, 0.00002718281828459045);      	
-		
+		String entero = String.format("%d", 11037);  
+		String flotante = String.format("%f %.10f", 3.141592653589793, 0.00002718281828459045);
+		String notacionCientificaComputarizada = String.format("%e %E", 3.141592653589793, 0.00002718281828459045);
+		String notacionCientificaGeneral = String.format("%g %G", 3.141592653589793, 0.00002718281828459045);
+
+		System.out.println("Entero:");
 		System.out.println(entero);
+
+		System.out.println("\nFlotante:");
 		System.out.println(flotante);
+
+		System.out.println("\nNotacion Cientifica Computarizada:");
 		System.out.println(notacionCientificaComputarizada);
+
+		System.out.println("\nNotacion Cientifica General:");
 		System.out.println(notacionCientificaGeneral);
 	}
 
 	public static void example2() {
 		String boleano = String.format("%b %B", true, false);
 		String caracter = String.format("%c %C ", 'a', 'z');
-        	String cadena = String.format("%s %S", "Madeline", "Badeline");
+		String cadena = String.format("%s %S", "Madeline", "Badeline");
 		
+		System.out.println("Booleano:");
 		System.out.println(boleano);
+
+		System.out.println("\nCaracter:");
 		System.out.println(caracter);
+
+		System.out.println("\nCadena:");
 		System.out.println(cadena);
 	}
 
@@ -32,6 +44,7 @@ public class FormatExample {
 		String indexOrdenado = String.format("%1$s %2$s %3$s %4$s %5$s", "a", "b", "c", "d", "e");
 		String indexDesordenado = String.format("%5$s %4$s %3$s %2$s %1$s", "a", "b", "c", "d", "e");
 		
+		System.out.println("Indice de argumentos:");
 		System.out.println(indexOrdenado);
 		System.out.println(indexDesordenado);
 	}
--- string/examples/SplitExample.java
+++ string/examples/SplitExample.java
@@ -1,12 +1,20 @@
+import java.util.Arrays;
+
+
 public class SplitExample {
 
 	public static void main(String[] args) {
-		String pi = "3-1-4-1-5-9";
+		String golden = "1-6-1-8-0-3-3-9-8-8-7-4-9";
 		String regex = new String("-");
-		String[] digits = pi.split(regex);
+		String[] digits = golden.split(regex);
 
-		for(String digit: digits)
-			System.out.println(digit);
+		System.out.println("Cadena original:");
+		System.out.println(golden);
+
+		System.out.println("\nArreglo:");
+		for(String digit : digits)
+			System.out.print(digit + " ");
+
+		System.out.println();
 	}
-
 }
